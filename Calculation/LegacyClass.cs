6827e7667bfaf96f6748f990fa9dd8a881924676
yontenc <yontenchoden1991@gmail.com>
2021-06-17 15:36:50 +0600

    Legacy code unit testing using Sprout method ans sprout class is implemented

---
+++ Calculation/LegacyClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalPhotos.LegacyProject
+{
+    public class LegacyClass
+    {
+        public void AppendItem<Tkey, TValue>(Dictionary<Tkey, TValue> fromDictionary,
+            Dictionary<Tkey, TValue> toDictionary)
+        {
+            Dictionary<Tkey, TValue> validItems = GetValidItems(toDictionary, fromDictionary);
+            foreach (var item in validItems)
+            { 
+                if(!toDictionary.ContainsKey(item.Key))
+                 toDictionary.Add(item.Key,item.Value); 
+                Console.WriteLine($"{item.Key}, {item.Value}");
+            }
+
+        }
+
+        public Dictionary<Tkey, TValue> GetValidItems<Tkey, TValue>( Dictionary<Tkey, TValue> fromDictionary, Dictionary<Tkey, TValue> toDictionary)
+        {
+            var result = new Dictionary<Tkey, TValue>();
+            foreach (var item in fromDictionary)
+            {
+                if(!toDictionary.ContainsKey(item.Key))
+                    result.Add(item.Key,item.Value);
+            }
+
+            return result;
+        }
+    }
+}
